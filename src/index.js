5f0eebdd19857c50a39132ba406c1eb1e6a7db5f
Harel Glazerman <arel0810g2@gmail.com>
2018-12-16 13:56:24 +0200

    robofriends-active search bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Card from './Card';
+import { robots } from './robots';
+import App from './App';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// <CardList robots={robots}/>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
